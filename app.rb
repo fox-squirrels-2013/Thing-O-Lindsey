207dcacbc90897e0f24cd1cd2a658394c86ca651
Lindsey Sugino <lindseysugino@gmail.com>
2013-10-28 18:00:56 -0700

    updated part 2

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'active_record'
+require_relative './app/models/users'
+
+ActiveRecord::Base.establish_connection(adapter: 'postgresql')
+										# database: 'users')
+
 # require_relative  #fill in 'model'
 
 get '/' do  
